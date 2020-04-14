1d553c94bdd9ec010f494ad0782dbf584da05d23
Kevin Boston <kbos22@gmail.com>
2020-04-14 15:43:39 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,10 @@
 // declare your functions here...
+function paragraphSelector() {
+  $("p");
+}
+function lastImageSelector(){
+  $('img:last');
+}
+function firstListItem(){
+  $('pic-list ul:first-child');
+}
